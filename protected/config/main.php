7d61a614f9d1b0db05fb30011459d82ba9061934
fbeans@gmail.com <fabio.carvalho@gmail.com>
2014-06-23 10:49:34 -0300

    Configuração de "linguagem" no main

--- protected/config/main.php
+++ protected/config/main.php
@@ -8,6 +8,9 @@
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'My Web Application',
+        
+        'sourceLanguage' => 'pt_br',
+         'language' => 'pt-br',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
